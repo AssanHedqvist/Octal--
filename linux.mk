c9cd4b6f107a66549e84b033102f7174b6eb6367
Sebastian Segerstein <sebastian_segerstein@hotmail.com>
2023-04-06 11:47:55 +0200

    Updated makefile for Linux

--- linux.mk
+++ linux.mk
@@ -1,3 +1,17 @@
 #	linux users fix this
 #	run this with 
 #   "make -f linux.mk"
+
+SRCDIR=./src
+
+CFLAGS = -g -c
+
+game: main.o
+	gcc -o game.exe main.o
+	
+main.o:
+	gcc -c $(SRCDIR)/main.c
+
+clean:
+	rm  main.o
+	rm  main
