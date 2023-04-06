0d52d23d8c289d468762b219f85e59159b7d46d0
wsjoden <wille@MacBook-Pro-som-tillhor-William.local>
2023-04-06 11:17:39 +0200

    fixed makefile for mac i think

--- mac.mk
+++ mac.mk
@@ -1,3 +1,32 @@
-#	mac users fix this
 #	run this with 
 #   "make -f mac.mk"
+
+# Location of where main.c is stored in
+SRCDIR=./source
+
+# Name of the compiler, GNU GCC in this case
+CC=gcc
+
+# Any include directories
+INCLUDE = /opt/homebrew/include
+
+# Any flags for the compiler
+CFLAGS = -g -I$(INCLUDE) -c
+#CFLAGS = -g -c
+
+# Any libraries to link with
+LIBS = /opt/homebrew/lib
+
+
+# Extra flags to give to compilers when they are supposed to invoke the linker
+#LDFLAGS = -lmingw32 -lSDL2main -lSDL2 -mwindows
+LDFLAGS = -lSDL2main -lSDL2 -lSDL2_image
+
+main:
+	@echo "Building main"
+	$(CC) $(CFLAGS) $(SRCDIR)/main.c -o $@.o 
+	$(CC) main.o -o main $(LDFLAGS) -L$(LIBS)
+
+clean:
+	rm -f main.o
+	rm -f main
