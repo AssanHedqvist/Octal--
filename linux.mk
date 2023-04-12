cd18b07dc474a0074eb062c7f7f0371d87b9a6c4
Sebastian Segerstein <sebastian_segerstein@hotmail.com>
2023-04-12 10:35:29 +0200

    Updated linux make file

--- linux.mk
+++ linux.mk
@@ -4,14 +4,17 @@
 
 SRCDIR=./src
 
+INCLUDE=./include
+
 CFLAGS = -g -c
 
-game: main.o
-	gcc -o game.exe main.o
+game: main.o vec2.o
+	gcc -o game.exe main.o vec2.o
+main.o: 
+	gcc -c $(SRCDIR)/main.c 
+vec2.o: 
+	gcc -c $(SRCDIR)/vec2.c
 	
-main.o:
-	gcc -c $(SRCDIR)/main.c
-
 clean:
-	rm  main.o
-	rm  main
+	rm *.exe
+	rm *.o
