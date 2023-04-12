f7cd23756182f822128163d142ee1a912792f24b
Damien Hausmanis Carlén <damienhc@kth.se>
2023-04-12 19:40:14 +0200

    added more than just macro so decided to add everything instead

---
+++ src/verlet.c
@@ -0,0 +1,18 @@
+#include "../include/verlet.h"
+
+void updateVerletPos(Verlet* obj, float dt) 
+{
+    vec2 velocity = vdiff(obj->posNow,obj->posOld);
+
+    obj->posOld = obj->posNow;
+
+    obj->posNow = vsum(vsum(obj->posNow,velocity),vsmul(obj->acceleration,dt*dt));
+
+    //obj->acceleration = vec2(0.f,0.f);
+}
+
+//  not needed
+void accelerate(Verlet* obj, vec2 acc) 
+{
+    obj->acceleration = vsum(obj->acceleration, acc);
+}
