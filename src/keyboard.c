1a987f5f8b64a3b8f35e53f6d54300a7f100d90d
Damien Hausmanis Carlén <damienhc@kth.se>
2023-04-12 20:29:48 +0200

    fixed a potential bug in keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,5 +3,9 @@
 void handleKeyboardInputs(KeyboardStates* states, SDL_Keycode keyCode, int eventType) 
 {
     //  this works because SDL_KEYUP - SDL_KEYUP = 0 and SDL_KEYUP - SDL_KEYDOWN = 1 
-    states->keyState[keyCode] = SDL_KEYUP - eventType;
+    
+    if(keyCode < 122) //  if true ignore that input
+    { 
+        states->keyState[keyCode] = SDL_KEYUP - eventType;
+    }
 }
