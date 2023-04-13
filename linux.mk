2fb9419d418f94181723fc741b0b3530e728756a
Sebastian Segerstein <sebastian_segerstein@hotmail.com>
2023-04-13 23:02:37 +0200

    Updated linux Makefile

--- linux.mk
+++ linux.mk
@@ -3,18 +3,18 @@
 #   "make -f linux.mk"
 
 SRCDIR=./src
-
 INCLUDE=./include
+INCLUDE2 = /usr/include/SDL2
+LDFLAGS = -lSDL2main -lSDL2 -lSDL2_image -lm
 
 CFLAGS = -g -c
 
 game: main.o vec2.o
-	gcc -o game.exe main.o vec2.o
+	gcc -o game.exe main.o vec2.o $(LDFLAGS)
 main.o: 
 	gcc -c $(SRCDIR)/main.c 
 vec2.o: 
 	gcc -c $(SRCDIR)/vec2.c
 	
 clean:
-	rm *.exe
-	rm *.o
+	rm -f *.exe *.o
