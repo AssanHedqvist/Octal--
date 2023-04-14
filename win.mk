4b9deca09d7e55954b422e7cdc4414b4192de711
Damien Hausmanis Carlén <damienhc@kth.se>
2023-04-14 10:40:54 +0200

    added semi-euler

--- win.mk
+++ win.mk
@@ -9,14 +9,14 @@ LIBS = C:\msys64\mingw64\lib
 
 LDFLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -mwindows
 
-game: main.o vec2.o verlet.o object.o
-	gcc -o game.exe main.o vec2.o verlet.o object.o -L$(LIBS) -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -mwindows
+game: main.o vec2.o integration.o object.o
+	gcc -o game.exe main.o vec2.o integration.o object.o -L$(LIBS) -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -mwindows
 main.o: 
 	gcc -c $(SRCDIR)/main.c -I$(INCLUDE)
 vec2.o: 
 	gcc -c $(SRCDIR)/vec2.c
-verlet.o: 
-	gcc -c $(SRCDIR)/verlet.c	
+integration.o: 
+	gcc -c $(SRCDIR)/integration.c	
 object.o:
 	gcc -c $(SRCDIR)/object.c -I$(INCLUDE)
 clean:
