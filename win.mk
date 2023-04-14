5cceaa5b1f35e18ab576de0d9e5520719dd66d09
Damien Hausmanis Carlén <damienhc@kth.se>
2023-04-14 15:21:58 +0200

    fixed windows make file

--- win.mk
+++ win.mk
@@ -9,16 +9,16 @@ LIBS = C:\msys64\mingw64\lib
 
 LDFLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -mwindows
 
-game: main.o vec2.o integration.o object.o
-	gcc -o game.exe main.o vec2.o integration.o object.o -L$(LIBS) -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -mwindows
+game: main.o vec2.o physicsObject.o renderObject.o
+	gcc -o game.exe main.o vec2.o physicsObject.o renderObject.o -L$(LIBS) -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -mwindows
 main.o: 
 	gcc -c $(SRCDIR)/main.c -I$(INCLUDE)
 vec2.o: 
 	gcc -c $(SRCDIR)/vec2.c
-integration.o: 
-	gcc -c $(SRCDIR)/integration.c	
-object.o:
-	gcc -c $(SRCDIR)/object.c -I$(INCLUDE)
+physicsObject.o: 
+	gcc -c $(SRCDIR)/physicsObject.c	
+renderObject.o:
+	gcc -c $(SRCDIR)/renderObject.c -I$(INCLUDE)
 clean:
 	rm *.o
 	rm *.exe
