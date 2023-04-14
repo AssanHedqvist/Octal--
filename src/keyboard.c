33bc2af6c05ad15341607715edc469f5d57727af
Damien Hausmanis Carlén <damienhc@kth.se>
2023-04-14 10:15:11 +0200

    fixade keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,8 +3,7 @@
 void handleKeyboardInputs(KeyboardStates* states, SDL_Keycode keyCode, int eventType) 
 {
     //  this works because SDL_KEYUP - SDL_KEYUP = 0 and SDL_KEYUP - SDL_KEYDOWN = 1 
-    
-    if(keyCode < 122) //  if true ignore that input
+    if(keyCode < 123) //  if true ignore that input
     { 
         states->keyState[keyCode] = SDL_KEYUP - eventType;
     }
