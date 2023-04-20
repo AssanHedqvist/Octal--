2ac2b30ee3017a66a2e3bb52d1a8e3788f95b0d0
wsjoden <wille@MBPsomtrWilliam.lan>
2023-04-20 12:45:13 +0200

    server makefile for mac

--- server/mac.mk
+++ server/mac.mk
@@ -1,3 +1,7 @@
-serverOchClient: udps.c udpc.c
-	gcc -o server udpsMirror.c -lSDL2_net
-	gcc -o client udpc.c -lSDL2_net
+#	run this with 
+#   "make -f mac.mk"
+#	may be runnable for all OS
+
+serverAndClient: server.c client.c
+	gcc -o server server.c -lSDL2_net
+	gcc -o client client.c -lSDL2_net
