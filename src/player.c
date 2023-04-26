704f7336a049143e51614abff3b0e86d99d2d6ec
Kohsar <Kohsar@kth.se>
2023-04-26 13:18:20 +0200

    add double jump

--- src/player.c
+++ src/player.c
@@ -1,8 +1,11 @@
 #include "../include/player.h"
 #include "../include/spriteHandler.h"
+#include <time.h>
 
 #define totSprites 3
-
+int latestJumpFromPlattform; // den här behövs för att annars kan man hoppa 3 gånger i rad
+static float timeSinceLastJump = 0.f;
+const float jumpCooldown = 0.1f;
 void handlePlayerInputs(Player *player, const float dt)
 {
     if (isKeyDown(&player->keyInputs,SDL_SCANCODE_A))
@@ -17,20 +20,29 @@ void handlePlayerInputs(Player *player, const float dt)
         player->physics->oldPos = vsum(player->physics->oldPos, vsmul(vec2(-100.0f, 0.f),dt));
     }
 
-    if (isKeyDown(&player->keyInputs,SDL_SCANCODE_SPACE))
+    if(player->physics->recentCollision == 1) 
     {
-        if(player->physics->recentCollision == 1) 
-        {
-            player->amountOfJumpsLeft = 2;
-        }
-        if (player->amountOfJumpsLeft > 0) 
-        {
-            player->physics->oldPos = vsum(player->physics->oldPos, vsmul(vec2(0.f, -196.4f),dt));
-            player->amountOfJumpsLeft--;
-        }
-       
+        if (latestJumpFromPlattform ==1) player->amountOfJumpsLeft = 1;
+        else player->amountOfJumpsLeft = 2;
+    }
+
+if (isKeyDown(&player->keyInputs,SDL_SCANCODE_SPACE) && timeSinceLastJump >= jumpCooldown)
+{
+    if (player->amountOfJumpsLeft > 0) 
+    {
+        vec2 velocity = vdiff(player->physics->pos, player->physics->oldPos);
+        player->physics->oldPos = vsum(vsum(player->physics->oldPos, vsmul(vec2(0.f, -396.4f),dt)), vec2(0.f, velocity.y));
+        if (player->physics->recentCollision == 1) latestJumpFromPlattform =1;
+        else latestJumpFromPlattform =0;
+        player->amountOfJumpsLeft--;
+        timeSinceLastJump = 0.f;
     }
 }
+
+timeSinceLastJump += dt;
+       
+    }
+
 
 void handlePlayerAnimation(Player *player)
 {
