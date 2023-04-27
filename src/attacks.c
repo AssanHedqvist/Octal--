3950b8941486582142c87391437037d65e89b8b2
wsjoden <sjoden.william@gmail.com>
2023-04-27 13:59:57 +0200

    failede min merge lite. fixat nu

--- src/attacks.c
+++ src/attacks.c
@@ -8,21 +8,19 @@ void lightPunch(Player players[], int amountOfPlayers)
     {
         for (int i = 1; i < 4; i++)
         {
-            int yDist = players[0].physics->pos.y - players[i].physics->pos.y;
-            int xDist = players[0].physics->pos.x - players[i].physics->pos.x;
+            //int yDist = players[0].physics->pos.y - players[i].physics->pos.y;
+            //int xDist = players[0].physics->pos.x - players[i].physics->pos.x;
             //int playerWidth = players->render->screenExtents.w;
             //int playerHeight = players->render->screenExtents.h;
 
-            if (xDist >= -(players->render->screenExtents.w) && xDist <= 0 && yDist <= (players->render->screenExtents.h / 2) && yDist >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 0) //   magic number has to be tweaked.
+            if ((players[0].physics->pos.x - players[i].physics->pos.x) >= -(players->render->screenExtents.w) && (players[0].physics->pos.x - players[i].physics->pos.x) <= 0 && (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 0) //   magic number has to be tweaked.
             {
                 players[i].health += 10;
             }
-            else if (xDist <= (players->render->screenExtents.w) && xDist >= 0 && yDist <= (players->render->screenExtents.h / 2) && yDist >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 1)
+            else if ((players[0].physics->pos.x - players[i].physics->pos.x) <= (players->render->screenExtents.w) && (players[0].physics->pos.x - players[i].physics->pos.x) >= 0 && (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 1)
             {
                 players[i].health += 10;
             }
-
-            printf("ydist: %d \n", yDist);
         }
         players->render->imageExtents.x = 96;
     }
