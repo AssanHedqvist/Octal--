355f8544a7e0405982d437cf87e0377be929e48f
wsjoden <sjoden.william@gmail.com>
2023-04-27 13:55:04 +0200

    facing attacks

--- src/attacks.c
+++ src/attacks.c
@@ -6,12 +6,23 @@ void lightPunch(Player players[], int amountOfPlayers)
 {
     if (isKeyDown(&players->keyInputs, SDL_SCANCODE_J))
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 1; i < 4; i++)
         {
-            if ((players[0].physics->pos.x - players[i].physics->pos.x) >  40) //   magic number has to be tweaked.
+            int yDist = players[0].physics->pos.y - players[i].physics->pos.y;
+            int xDist = players[0].physics->pos.x - players[i].physics->pos.x;
+            //int playerWidth = players->render->screenExtents.w;
+            //int playerHeight = players->render->screenExtents.h;
+
+            if (xDist >= -(players->render->screenExtents.w) && xDist <= 0 && yDist <= (players->render->screenExtents.h / 2) && yDist >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 0) //   magic number has to be tweaked.
             {
                 players[i].health += 10;
             }
+            else if (xDist <= (players->render->screenExtents.w) && xDist >= 0 && yDist <= (players->render->screenExtents.h / 2) && yDist >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 1)
+            {
+                players[i].health += 10;
+            }
+
+            printf("ydist: %d \n", yDist);
         }
         players->render->imageExtents.x = 96;
     }
