296862e090d50f5b3d751ae5639e35560efceab7
unknown <secka@kth.se>
2023-05-01 16:24:45 +0200

    small changes

--- src/menu.c
+++ src/menu.c
@@ -9,6 +9,10 @@
 SDL_Rect* renderMenu(SDL_Renderer* renderer, Text menuText)
 {
     SDL_Rect* buttonRects = malloc(sizeof(SDL_Rect) * 3);
+    buttonRects[0] = (SDL_Rect){BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT};
+    buttonRects[1] = (SDL_Rect){BUTTON_X, BUTTON_Y+60, BUTTON_WIDTH, BUTTON_HEIGHT};
+    buttonRects[2] = (SDL_Rect){BUTTON_X, BUTTON_Y+120, BUTTON_WIDTH, BUTTON_HEIGHT};
+
     SDL_Color textColor = {255, 255, 255, 155};
     menuText.font = TTF_OpenFont("./resources/fonts/LuckiestGuy.ttf", 100);
 
@@ -21,27 +25,21 @@ SDL_Rect* renderMenu(SDL_Renderer* renderer, Text menuText)
     //render button1
     SDL_Surface* button1 = TTF_RenderText_Solid(menuText.font, "Play", textColor);
     texture = SDL_CreateTextureFromSurface(renderer, button1);
-    SDL_Rect buttonRect1 = {BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT};
-    SDL_RenderCopyEx(renderer, texture, NULL, &buttonRect1, 0.0, NULL, 0);
-    buttonRects[0] = buttonRect1;
+    SDL_RenderCopyEx(renderer, texture, NULL, &buttonRects[0], 0.0, NULL, 0);
     SDL_FreeSurface(button1);
     SDL_DestroyTexture(texture);
 
     //render button2
-    SDL_Surface* button2 = TTF_RenderText_Solid(menuText.font, "Button2", textColor);
+    SDL_Surface* button2 = TTF_RenderText_Solid(menuText.font, "EMPTY", textColor);
     texture = SDL_CreateTextureFromSurface(renderer, button2);
-    SDL_Rect buttonRect2 = {BUTTON_X, BUTTON_Y+60, BUTTON_WIDTH, BUTTON_HEIGHT};
-    SDL_RenderCopyEx(renderer, texture, NULL, &buttonRect2, 0.0, NULL, 0);
-    buttonRects[1] = buttonRect2;
+    SDL_RenderCopyEx(renderer, texture, NULL, &buttonRects[1], 0.0, NULL, 0);
     SDL_FreeSurface(button2);
     SDL_DestroyTexture(texture);
 
     //render button3
     SDL_Surface* button3 = TTF_RenderText_Solid(menuText.font, "Quit", textColor);
     texture = SDL_CreateTextureFromSurface(renderer, button3);
-    SDL_Rect buttonRect3 = {BUTTON_X, BUTTON_Y+120, BUTTON_WIDTH, BUTTON_HEIGHT};
-    SDL_RenderCopyEx(renderer, texture, NULL, &buttonRect3, 0.0, NULL, 0);
-    buttonRects[2] = buttonRect3;
+    SDL_RenderCopyEx(renderer, texture, NULL, &buttonRects[2], 0.0, NULL, 0);
     SDL_FreeSurface(button3);
     SDL_DestroyTexture(texture);
 
