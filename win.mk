46d0c7b0e41fb78882706f1faa660375452bc8a7
Damien Hausmanis Carlén <damienhc@kth.se>
2023-05-02 15:03:07 +0200

    windows makefile fixed

--- win.mk
+++ win.mk
@@ -9,15 +9,15 @@ LIBS = C:\msys64\mingw64\lib
 
 LDFLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_net -lSDL2_ttf -lSDL2_mixer -mwindows
 
-game: main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o
-	gcc -o game.exe main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o -L$(LIBS) $(LDFLAGS)
+game: main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o mouse.o
+	gcc -o game.exe main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o mouse.o -L$(LIBS) $(LDFLAGS)
 main.o: 
 	gcc -c $(SRCDIR)/main.c -I$(INCLUDE)
 vec2.o: 
 	gcc -c $(SRCDIR)/vec2.c
 keyboard.o:
 	gcc -c $(SRCDIR)/keyboard.c -I$(INCLUDE)
-physicsObject.o: vec2.o
+physicsObject.o: vec2.o 
 	gcc -c $(SRCDIR)/physicsObject.c	
 renderObject.o:
 	gcc -c $(SRCDIR)/renderObject.c -I$(INCLUDE)
@@ -31,6 +31,8 @@ sounds.o:
 	gcc -c $(SRCDIR)/sounds.c -I$(INCLUDE)
 menu.o:
 	gcc -c $(SRCDIR)/menu.c -I$(INCLUDE)
+mouse.o:
+	gcc -c $(SRCDIR)/mouse.c -I$(INCLUDE)
 
 clean:
 	rm *.o
