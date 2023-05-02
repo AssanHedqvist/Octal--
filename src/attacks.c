3432b0479495db5a0120c2bf5a8df12c516e4f74
wsjoden <sjoden.william@gmail.com>
2023-05-02 13:54:08 +0200

    acceleration when hit

--- src/attacks.c
+++ src/attacks.c
@@ -9,29 +9,37 @@ void lightPunch(Player players[], int amountOfPlayers)
         for (int i = 1; i < 4; i++)
         {
             //   I'll keep those here to clarify the math for myself. Will delete in a week or so.
+            
             //int yDist = players[0].physics->pos.y - players[i].physics->pos.y;
             //int xDist = players[0].physics->pos.x - players[i].physics->pos.x;
             //int playerWidth = players->render->screenExtents.w;
             //int playerHeight = players->render->screenExtents.h;
 
-            if ((players[0].physics->pos.x - players[i].physics->pos.x) + (players->render->screenExtents.w * 0.2) >= -(players->render->screenExtents.w) && (players[0].physics->pos.x - players[i].physics->pos.x) <= 0 && (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 0) //   magic number has to be tweaked.
+            if ((players[0].physics->pos.x - players[i].physics->pos.x) +
+            (players->render->screenExtents.w * 0.2) >= -(players->render->screenExtents.w) && 
+            (players[0].physics->pos.x - players[i].physics->pos.x) <= 0 && 
+            (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && 
+            (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) &&
+            players[0].render->flip == 0)
             {
                 players[i].health += 10;
+                players[i].physics->acceleration.x += 500;
             }
-            else if ((players[0].physics->pos.x - players[i].physics->pos.x) - (players->render->screenExtents.w * 0.2) <= (players->render->screenExtents.w) && (players[0].physics->pos.x - players[i].physics->pos.x) >= 0 && (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 1)
+            else if ((players[0].physics->pos.x - players[i].physics->pos.x) - 
+            (players->render->screenExtents.w * 0.2) <= (players->render->screenExtents.w) && 
+            (players[0].physics->pos.x - players[i].physics->pos.x) >= 0 && 
+            (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && 
+            (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && 
+            players[0].render->flip == 1)
             {
                 players[i].health += 10;
+                players[i].physics->acceleration.x -= 500;
+              
             }
-
-            /*if ((players[0].physics->pos.x - players[i].physics->pos.x) >= -(players->render->screenExtents.w) && (players[0].physics->pos.x - players[i].physics->pos.x) <= 0 && (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 0) //   magic number has to be tweaked.
-            {
-                players[i].health += 10;
-            }
-            else if ((players[0].physics->pos.x - players[i].physics->pos.x) <= (players->render->screenExtents.w) && (players[0].physics->pos.x - players[i].physics->pos.x) >= 0 && (players[0].physics->pos.y - players[i].physics->pos.y) <= (players->render->screenExtents.h / 2) && (players[0].physics->pos.y - players[i].physics->pos.y) >= -(players->render->screenExtents.h / 2) && players[0].render->flip == 1)
-            {
-                players[i].health += 10;
-            }*/
         }
         players->render->imageExtents.x = 96;
     }
 }
+
+
+
