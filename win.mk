9a9dc2396ebc826d6bfa1cbeb67fe140d6493c30
Damien Hausmanis Carlén <damienhc@kth.se>
2023-05-03 09:51:26 +0200

    input lag is fixed just needed to get all packets XD

--- win.mk
+++ win.mk
@@ -10,7 +10,7 @@ LIBS = C:\msys64\mingw64\lib
 LDFLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_net -lSDL2_ttf -lSDL2_mixer -mwindows
 
 game: main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o mouse.o
-	gcc -o game.exe main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o mouse.o -L$(LIBS) $(LDFLAGS)
+	gcc -o game.exe -O2 main.o vec2.o physicsObject.o renderObject.o keyboard.o player.o text.o attacks.o sounds.o menu.o mouse.o -L$(LIBS) $(LDFLAGS)
 main.o: 
 	gcc -c $(SRCDIR)/main.c -I$(INCLUDE)
 vec2.o: 
